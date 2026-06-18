2447449cc39733bc0d9afd243669853768d2b3cb
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-06-18 23:12:57 +0530

    Search insert positoin

---
+++ 4_Searching/1_Easy/2_Search Insert Position/Sip.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/search-insert-position/description/
+public class Sip {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
