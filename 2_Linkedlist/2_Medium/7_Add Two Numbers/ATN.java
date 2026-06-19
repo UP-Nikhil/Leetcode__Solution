5891fab3fc1c6ae814ecce1417938235e6362cfd
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-06-19 17:26:38 +0530

    add two number

---
+++ 2_Linkedlist/2_Medium/7_Add Two Numbers/ATN.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/add-two-numbers/
+public class ATN {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode extra = new ListNode(0);
+        ListNode temp = extra;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+        }
+        if (carry > 0) {
+            temp.next = new ListNode(carry);
+        }
+        return extra.next;
+
+    }
+}
