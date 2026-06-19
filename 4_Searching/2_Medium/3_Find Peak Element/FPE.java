7731229d232d78fc96a11a788a08ddd2b4884f35
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-06-19 17:02:48 +0530

    find peek element

---
+++ 4_Searching/2_Medium/3_Find Peak Element/FPE.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/find-peak-element/description/
+public class FPE {
+    public static int findPeakElement(int[] nums) {
+                int l = 0;
+                int r = nums.length - 1;
+                while (l < r) {
+                    int mid = l + (r - l) / 2;
+                    if (nums[mid] < nums[mid + 1]) {
+                        l = mid + 1;
+                    } else {
+                        r = mid;
+                    }
+
+                }
+                return l;
+            }
+
+        
+    
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 1 };
+        int ans = findPeakElement(arr);
+        System.out.println(ans);
+    }
+}
