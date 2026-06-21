cf0d8f98981149fe5ff9fb2eaf1554b558b3d4e6
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-06-21 12:09:21 +0530

    new changes

--- 3_String/1_Easy/4_Reverse Prefix of Word/RPW.java
+++ 3_String/1_Easy/4_Reverse Prefix of Word/RPW.java
@@ -1,29 +1,30 @@
-
+//https://leetcode.com/problems/reverse-prefix-of-word/description/
 
 public class RPW {
-     public static  String reversePrefix(String word, char ch) {
-        int f=0;
-        int l=0;
-        char arr[]= word.toCharArray();
-        for(int i=0; i< arr.length; i++){
-            if(arr[i]==ch){
-                l=i;
+    public static String reversePrefix(String word, char ch) {
+        int f = 0;
+        int l = 0;
+        char arr[] = word.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ch) {
+                l = i;
                 break;
             }
         }
-        while(f < l){
-            char temp= arr[f];
-            arr[f]=arr[l];
-            arr[l]= temp;
+        while (f < l) {
+            char temp = arr[f];
+            arr[f] = arr[l];
+            arr[l] = temp;
             l--;
             f++;
         }
         return new String(arr);
-        
+
     }
+
     public static void main(String[] args) {
-         String word = "abcdefd";
-         char d= 'd';
-         System.out.println(reversePrefix(word,d));
+        String word = "abcdefd";
+        char d = 'd';
+        System.out.println(reversePrefix(word, d));
     }
 }
