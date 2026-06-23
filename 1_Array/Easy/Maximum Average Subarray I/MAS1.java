061777a5b36a3c495a25ae4b0f547e1a0a18a639
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-06-23 16:20:08 +0530

    find error;

--- 1_Array/Easy/Maximum Average Subarray I/MAS1.java
+++ 1_Array/Easy/Maximum Average Subarray I/MAS1.java
@@ -1,32 +1,32 @@
 // https://leetcode.com/problems/maximum-average-subarray-i/description/?envType=problem-list-v2&envId=sliding-window
 
 public class MAS1 {
-    public  static double findMaxAverage(int[] nums, int k) {
+    public static double findMaxAverage(int[] nums, int k) {
 
         int sum = 0;
-        double avg = 0, maxAvg;
 
         for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
 
-        maxAvg = (double) sum / k;
+        double maxAvg = (double) sum / k;
 
         for (int j = k; j < nums.length; j++) {
 
             sum = sum + nums[j];
             sum = sum - nums[j - k];
 
-            avg = (double) sum / k;
+            double avg = (double) sum / k;
             maxAvg = Math.max(maxAvg, avg);
         }
 
         return maxAvg;
     }
+
     public static void main(String[] args) {
         int arr[] = { 2, 7, 11, 15 };
-        int k=2;
-        double ans= findMaxAverage(arr, k);
+        int k = 2;
+        double ans = findMaxAverage(arr, k);
         System.out.println(ans);
     }
 }
