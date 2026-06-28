ae5b46e581595c32fa92451a16a903cecbc5bcae
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-06-28 23:16:21 +0530

    format change

--- 1_Array/Medium/0_Matrix/1_Rotate Image/RI.java
+++ 1_Array/Medium/0_Matrix/1_Rotate Image/RI.java
@@ -1,24 +1,24 @@
 //https://leetcode.com/problems/rotate-image/description/
 
 public class RI {
-     public void rotate(int[][] matrix) {
+    public void rotate(int[][] matrix) {
         // transpose
         int n = matrix.length;
-         for(int i=0; i< n; i++){
-             for(int j =i; j< n; j++){
-                 int temp = matrix[i][j];
-                 matrix[i][j] = matrix[j][i];
-                 matrix[j][i] = temp;
-             }
-         }
+        for(int i=0; i< n; i++){
+            for(int j =i; j< n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
         // reverse
         for(int i =0; i<n; i++){
             int l=0;
             int r = matrix[0].length -1;
             while(l<r){
                 int temp = matrix[i][l];
-                 matrix[i][l] = matrix[i][r];
-                 matrix[i][r] = temp;
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
                 l++;
                 r--;
             }
