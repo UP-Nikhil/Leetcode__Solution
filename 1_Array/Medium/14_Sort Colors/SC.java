9433b7383a82fa7c5b5cdfb8c39ea067ec321dfb
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-06-30 22:28:08 +0530

    sort colors

---
+++ 1_Array/Medium/14_Sort Colors/SC.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sort-colors/description/
+import java.util.*;
+class SC{
+     public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        
+    }
+     public static void main(String[] args) {
+        
+    }
+}
