eb81395551f75256b41b173e66a3dc0d0aefcab8
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-07-02 15:06:43 +0530

    optimize solution

--- 1_Array/Medium/13_ Find First and Last Position of Element in Sorted Array/FFLE.java
+++ 1_Array/Medium/13_ Find First and Last Position of Element in Sorted Array/FFLE.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 public class FFLE {
-     public int[] searchRange(int[] nums, int target) {
+    /* public int[] searchRange(int[] nums, int target) {
 
         int first = -1;
         int last = -1;
@@ -18,6 +18,61 @@ public class FFLE {
         }
 
         return new int[]{first, last};
+    }
+        */
+
+        public int[] searchRange(int[] nums, int target) {
+
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int findLast(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
     }
     public static void main(String[] args) {
         
