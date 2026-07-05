a10a011c72337fc1373023d3ba956eda13221bf0
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-07-05 21:59:46 +0530

    valid pranthisis

---
+++ 6_Stack/1_Valid Pranthisis/VP.java
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+import java.util.*;
+public class VP {
+ 
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } 
+            else {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                char top = st.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
+
