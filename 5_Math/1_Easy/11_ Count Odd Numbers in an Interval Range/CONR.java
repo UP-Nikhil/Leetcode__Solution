3b775220643213ffeefc664f50250af8ac989162
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-07-26 12:20:18 +0530

    cont odd number in interval

---
+++ 5_Math/1_Easy/11_ Count Odd Numbers in an Interval Range/CONR.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/
+
+public class CONR {
+     public int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
