e113abfa84f778ecc9db5d74860bce2450d9af86
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-08-05 17:51:52 +0530

    stone game

---
+++ 1_Array/Medium/18_Stone Game/SG.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/stone-game/description/
+
+public class SG{
+     public boolean stoneGame(int[] piles) {
+        return true;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
