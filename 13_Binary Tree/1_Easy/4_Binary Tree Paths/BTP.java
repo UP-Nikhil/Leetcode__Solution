ab32579c54d27b74bfd3d12f61e0f28bee7225b2
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-08-05 18:13:34 +0530

    Binary tree path

---
+++ 13_Binary Tree/1_Easy/4_Binary Tree Paths/BTP.java
@@ -0,0 +1,46 @@
+//https://leetcode.com/problems/binary-tree-paths/description/
+
+import java.util.*;
+public class BTP {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private void helper(TreeNode root, List<String> ans, String s) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null) {
+            s += root.val;
+            ans.add(s);
+            return;
+        }
+        helper(root.left, ans, s + root.val + "->");
+        helper(root.right, ans, s + root.val + "->");
+
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        helper(root, ans, "");
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
