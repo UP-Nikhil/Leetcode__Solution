d73a569d4ce2d9529d2df9f9af620ac96c48c189
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-08-10 18:54:42 +0530

    validate Binary search Tree

---
+++ 13_Binary Tree/2_Medium/6_Validate Binary Search Tree/VBST.java
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/validate-binary-search-tree/description/
+public class VBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        private boolean helper(TreeNode root, long max, long min) {
+
+            if (root == null) {
+                return true;
+            }
+            if (root.val >= max || root.val <= min) {
+                return false;
+            }
+            return helper(root.left, root.val, min) && helper(root.right, max, root.val);
+        }
+
+        public boolean isValidBST(TreeNode root) {
+
+            return helper(root, Long.MAX_VALUE, Long.MIN_VALUE);
+        }
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
