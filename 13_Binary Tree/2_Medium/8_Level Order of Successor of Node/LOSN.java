473ae0758e197c696248cf0d4730a050ad397879
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-08-10 19:24:33 +0530

    level of succesor

---
+++ 13_Binary Tree/2_Medium/8_Level Order of Successor of Node/LOSN.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class LOSN {
+    public static TreeNode findSuccessor(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode currentNode = queue.poll();
+
+            if (currentNode.left != null) {
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.offer(currentNode.right);
+            }
+
+            if (currentNode.val == key) {
+                break;
+            }
+        }
+
+        return queue.peek();
+    }
+
+    // 2. टेस्ट करने के लिए main मेथड
+    public static void main(String[] args) {
+        /*
+         ये एक सैंपल ट्री बनाएगा:
+                1
+               / \
+              2   3
+             / \
+            4   5
+        */
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        int searchKey = 3; // हम 3 का अगला (successor) ढूंढ रहे हैं
+        TreeNode result = findSuccessor(root, searchKey);
+
+        if (result != null) {
+            System.out.println(searchKey + " Successor  " + result.val);
+        } else {
+            System.out.println(searchKey + " Successor not availabel ");
+        }
+    }
+}
