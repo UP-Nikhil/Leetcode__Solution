9eb6257dc8aaae6fe1b866c499569044c3c415f6
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-08-11 20:05:40 +0530

    Convert Sorted Array to Binary Search Tree

---
+++ 13_Binary Tree/1_Easy/11_Convert Sorted Array to Binary Search Tree/CSABS.java
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+public class CSABS {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        private TreeNode helper(int nums[], int start, int end) {
+            if (start > end) {
+                return null;
+            }
+            int mid = (start + end) / 2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = helper(nums, start, mid - 1);
+            root.right = helper(nums, mid + 1, end);
+            return root;
+
+        }
+
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return helper(nums, 0, nums.length - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
