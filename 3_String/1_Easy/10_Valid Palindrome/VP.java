b4e045e5c5480653cc63eddf5a162770540dd479
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-09-08 15:57:34 +0530

    isPalindrome

---
+++ 3_String/1_Easy/10_Valid Palindrome/VP.java
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/valid-palindrome/description/
+
+public class VP {
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+
+            if (!Character.isLetterOrDigit(left)) {
+                i++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(right)) {
+                j--;
+                continue;
+            }
+            if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+        String s = "A man, a plan, a canal: Panama";
+
+        System.out.println(isPalindrome(s));
+
+    }
+}
