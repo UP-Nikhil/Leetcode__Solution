cc2f1ec91e66e5096a8a29fb0f1dca2a85801373
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-09-15 19:12:26 +0530

    Repeated DNA Sequences

---
+++ 3_String/2_Medium/9_Repeated DNA Sequences/RDS.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/repeated-dna-sequences/description/
+
+import java.util.*;
+
+public class RDS {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> set = new HashSet<>();
+        HashSet<String> ans = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String dna = s.substring(i, i + 10);
+            if (set.contains(dna)) {
+                ans.add(dna);
+            } else {
+                set.add(dna);
+            }
+        }
+
+        return new ArrayList<>(ans);
+    }
+
+    public static void main (String args[]){
+        
+    }
+}
