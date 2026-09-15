11d68b27a84944441836ef82d7aca921f4d9874e
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-09-15 19:28:02 +0530

    Minimum Size Subarray Sum

---
+++ 1_Array/Medium/21_Minimum Size Subarray Sum/MSS.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/minimum-size-subarray-sum/description/
+
+public class MSS {
+
+    public int minSubArrayLen(int target, int[] nums) {
+
+        // Dynamic Sliding window;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int i = 0,
+                j = 0;
+        while (j < nums.length) {
+            sum += nums[j];
+            while (sum >= target) {
+                min = Math.min(min, j - i + 1);
+                sum -= nums[i];
+                i++;
+            }
+            j++;
+        }
+
+        if (min == Integer.MAX_VALUE)
+            return 0;
+        return min;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
