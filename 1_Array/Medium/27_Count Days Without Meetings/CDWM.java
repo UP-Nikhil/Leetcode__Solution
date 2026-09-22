068cbe67e1a4acf73f1eab81f1439151d950fe91
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-09-22 12:46:14 +0530

    update optimal approach

--- 1_Array/Medium/27_Count Days Without Meetings/CDWM.java
+++ 1_Array/Medium/27_Count Days Without Meetings/CDWM.java
@@ -2,7 +2,7 @@
 
  import java.util.*;
 public class CDWM {
-
+/*
     public int countDays(int days, int[][] meeting) {
 
         Arrays.sort(meeting, (a, b) -> Integer.compare(a[0], b[0]));
@@ -23,18 +23,48 @@ public class CDWM {
                 list.add(current);
             }
         }
+   
+        // int gap = 0;
+        // for (int i = 1; i < list.size(); i++) {
+        //     gap += list.get(i)[0] - list.get(i - 1)[1] - 1;
+        // }
 
-        int gap = 0;
-        for (int i = 1; i < list.size(); i++) {
-            gap += list.get(i)[0] - list.get(i - 1)[1] - 1;
+        // gap = gap + list.get(0)[0] -1;
+        // gap = gap + days - list.get(list.size() - 1)[1];
+
+        // return gap;
+
+          int freedays = 0;
+        for (int i = 0; i < list.size(); i++) {
+            freedays += list.get(i)[1] - list.get(i)[0] + 1;
         }
+        freedays = days - freedays;
+        return freedays;
 
-        gap = gap + list.get(0)[0] -1;
-        gap = gap + days - list.get(list.size() - 1)[1];
-
-        return gap;
 
     }
+*/
+      public int countDays(int days, int[][] meeting) {
+
+        Arrays.sort(meeting, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int gap =0; 
+        int maxEnd = meeting[0][1];
+
+        for(int i=1; i< meeting.length; i++){
+            if(meeting[i][0] > maxEnd){
+                gap += meeting[i][0] - maxEnd -1;
+            }
+
+            maxEnd = Math.max(meeting[i][1] , maxEnd);
+        }
+
+        gap = gap + meeting[0][0] -1;  // stating se kitna gap hai;
+        gap = gap + days - maxEnd;
+        
+        return gap;
+    }
+
     public static void main(String args[]){
 
     }
