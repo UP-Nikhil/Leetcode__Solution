30f679a8f071fe4aa5bab6b09a8e418e68c1285a
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-09-22 12:49:05 +0530

    Insert Interval

---
+++ 1_Array/Medium/28_Insert Interval/II.java
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/insert-interval/description/
+
+import java.util.*;
+public class II {
+   
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        List<int[]> list = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            list.add(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        list.add(newInterval);
+
+        while (i < intervals.length) {
+            list.add(intervals[i]);
+            i++;
+        }
+
+        // int[][] ans = new int[list.size()][];
+
+        // for (int j = 0; j < list.size(); j++) {
+        //     ans[j] = list.get(j);
+        // }
+
+        // return ans;
+
+       return list.toArray(new int[list.size()][2] );
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+
